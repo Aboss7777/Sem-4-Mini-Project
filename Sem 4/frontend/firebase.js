3da714229c118822753681f7e8f8fc43cb7ff42e
Aboss7777 <96716605+Aboss7777@users.noreply.github.com>
2023-04-20 20:32:06 +0530

    Add files via upload

---
+++ Sem 4/frontend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD_hEhGtGZbrjfsPQNQz6pYTREOTrlOWMY",
+    authDomain: "attendance-system-aaa6a.firebaseapp.com",
+    projectId: "attendance-system-aaa6a",
+    storageBucket: "attendance-system-aaa6a.appspot.com",
+    messagingSenderId: "397785923030",
+    appId: "1:397785923030:web:0ce7fd0648177bad203877"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
