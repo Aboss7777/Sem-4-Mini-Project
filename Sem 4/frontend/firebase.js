dd1f329866a27fd2a8d8b38645038b4889d037b3
Aboss7777 <96716605+Aboss7777@users.noreply.github.com>
2023-04-22 16:16:54 +0530

    Update firebase.js

--- Sem 4/frontend/firebase.js
+++ Sem 4/frontend/firebase.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+var firebaseConfig = {
+  databaseURL: "https://console.firebase.google.com/project/attendance-system-aaa6a/firestore/data",
+  apiKey: "AIzaSyD_hEhGtGZbrjfsPQNQz6pYTREOTrlOWMY",
+  authDomain: "attendance-system-aaa6a.firebaseapp.com",
+  projectId: "attendance-system-aaa6a",
+  storageBucket: "attendance-system-aaa6a.appspot.com",
+  messagingSenderId: "397785923030",
+  appId: "1:397785923030:web:0ce7fd0648177bad203877"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyD_hEhGtGZbrjfsPQNQz6pYTREOTrlOWMY",
-    authDomain: "attendance-system-aaa6a.firebaseapp.com",
-    projectId: "attendance-system-aaa6a",
-    storageBucket: "attendance-system-aaa6a.appspot.com",
-    messagingSenderId: "397785923030",
-    appId: "1:397785923030:web:0ce7fd0648177bad203877"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
